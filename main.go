00096b5ea77c9c0ea77e31b07200663811e1338a
zhangxuexian <xuexian.zhang@xtaotech.com>
2023-05-11 10:49:54 +0800

    搁置

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"github.com/cihub/seelog"
+)
 
 func main() {
+	configFilePath := flag.String("C", "conf/conf.yaml", "config file path")
+	logConfigPath := flag.String("L", "conf/seelog.xml", "log config file path")
+	flag.Parse()
+
+	logger, err := seelog.LoggerFromConfigAsFile(*logConfigPath)
+	if err != nil {
+		seelog.Critical("err parsing config log file", err)
+		return
+	}
+	seelog.ReplaceLogger(logger)
+	defer seelog.Flush()
+
+	//if err := system.
+
+	fmt.Println(*configFilePath, *logConfigPath)
 	fmt.Println("HI")
 }
